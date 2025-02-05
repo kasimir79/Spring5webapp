290ee371736d54d679a1d1d079bc8be39752509a
Kasimir Halitchi <kasimir.romania@gmail.com>
2025-02-05 20:06:37 +0200

    small update

--- src/main/java/guru/springframework/spring5webapp/OperationsConsoleApp.java
+++ src/main/java/guru/springframework/spring5webapp/OperationsConsoleApp.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
-@Component
+//@Component
 public class OperationsConsoleApp implements CommandLineRunner {
 
     private final OperationsService operationsService;
